4481a40591b600b19dfe7133e551d4fc04cd3aad
WaelAbuaishahNabed <wael.abuaishah@nabed.net>
2019-10-10 18:42:03 +0300

    Fix issue with getting dummy data

--- app/src/main/java/nabed/apps/nabedutils/MainActivity.kt
+++ app/src/main/java/nabed/apps/nabedutils/MainActivity.kt
@@ -2,11 +2,42 @@ package nabed.apps.nabedutils
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.IdRes
+import androidx.annotation.Nullable
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.activity_main.*
+import nabed.apps.nabedutilslibrary.views.prescriptions.NabedCategoriesView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        checkNabedExpiry.setOnClickListener {
+//            addNabedScreen()
+        }
     }
+
+//    private fun addNabedScreen() {
+//        replaceFragment(
+//            R.id.nabed_container,
+//            NabedCategoriesView.newInstance(),
+//            "NabedChapter",
+//            "state"
+//        )
+//    }
+//
+//    private fun replaceFragment(
+//        @IdRes containerViewId: Int,
+//        fragment: Fragment,
+//        fragmentTag: String,
+//        @Nullable backStackStateName: String
+//    ) {
+//        supportFragmentManager
+//            .beginTransaction()
+//            .replace(containerViewId, fragment, fragmentTag)
+//            .addToBackStack(backStackStateName)
+//            .commit()
+//    }
 }
